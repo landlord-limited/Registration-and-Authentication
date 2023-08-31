837884625cb220d974c1ae841e663c2f2a4e7cf6
Joy <soniqjjj@gmail.com>
2023-08-31 13:06:54 +0100

    Login.js

---
+++ Login.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { useAuth } from '../contexts/AuthContext';
+
+function Login() {
+  const { login } = useAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await login(email, password);
+      // Redirect to the dashboard or show a success message
+    } catch (error) {
+      // Handle login error
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
