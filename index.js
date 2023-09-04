ae9bd0d4b5168fb4aff777264ad011e7515e5a4f
Joy <soniqjjj@gmail.com>
2023-09-04 05:43:34 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import firebase from 'firebase/app';
+
+// Your Firebase config object goes here
+const firebaseConfig = {
+  // ... (copy the configuration object)
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// ... rest of your React app initialization code
